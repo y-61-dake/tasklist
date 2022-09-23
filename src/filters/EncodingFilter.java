a0eae83a2d4e23880aff28ec42e2034cc2dc5775
y-61- <y-61-@192.168.11.63>
2022-09-23 21:20:54 +0900

    NewServlet.javaを作成

---
+++ src/filters/EncodingFilter.java
@@ -0,0 +1,52 @@
+package filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+
+
+/**
+ * Servlet implementation class EncodingFilter
+ */
+@WebServlet("/*")
+public class EncodingFilter implements Filter {
+
+
+    /**
+     * Default constructor.
+     */
+    public EncodingFilter() {
+
+    }
+
+    /**
+     * @see Filter#destroy()
+     */
+    public void destroy() {
+
+    }
+
+    /**
+     * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)ServletRequest, ServletResponse, FilterChain)
+     */
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+
+        chain.doFilter(request, response);
+    }
+
+    /**
+     * @see Filter#init(FilterConfig)FilterConfig)
+     */
+    public void init(FilterConfig fConfig) throws ServletException {
+
+    }
+
+}
