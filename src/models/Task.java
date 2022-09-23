c23d52a8403b80791a066c69b4c25b04b8e22ed3
y-61- <y-61-@192.168.11.63>
2022-09-23 20:56:20 +0900

    getAllMessagesを追加

--- src/models/Task.java
+++ src/models/Task.java
@@ -7,24 +7,33 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
+@NamedQueries({
+    @NamedQuery(
+        name = "getAllTasks",
+        query = "SELECT m FROM Task AS m ORDER BY m.id DESC"
+    )
+})
 @Table(name = "tasks")
-public class Tasklist {
+public class Task {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column(name = "content", length = 255, nullable = false)
+    private String content;
+
     @Column(name = "created_at", nullable = false)
     private Timestamp created_at;
 
-    @Column(name = "updatde_at", nullable = false)
+    @Column(name = "updated_at", nullable = false)
     private Timestamp updated_at;
 
-    @Column(name = "content", length = 255, nullable = false)
-    private String content;
 
     public Integer getId() {
         return id;
@@ -32,6 +41,14 @@ public class Tasklist {
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
     }
 
     public Timestamp getCreated_at() {
@@ -48,14 +65,6 @@ public class Tasklist {
 
     public void setUpdated_at(Timestamp updated_at) {
         this.updated_at = updated_at;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
     }
 
 
